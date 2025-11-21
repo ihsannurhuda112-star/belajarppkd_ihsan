3365abc0a90246662aca856e35ae752df40c09f9
ihsannurhuda112-star <ihsannurhuda112@gmail.com>
2025-11-21 14:41:42 +0700

    hebat

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:belajarppkd_ihsan/day_33/views/splash_screen33.dart';
+import 'package:belajarppkd_ihsan/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   await initializeDateFormatting('id_ID', null);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
